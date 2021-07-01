d6ad6805b108cca26b0024e858dd68c8c1728077
Bobmonga <doctorhodges@gmail.com>
2021-07-01 08:33:38 +0100

    Check for no file on request.

--- src/MeterReadings.API/Controllers/ReadingController.cs
+++ src/MeterReadings.API/Controllers/ReadingController.cs
@@ -29,6 +29,11 @@ namespace MeterReadings.API.Controllers
         [HttpPost()]
         public IActionResult OnPostUpload(IFormFile meterReadingsFile)
         {
+            if (meterReadingsFile == null)
+            {
+                return BadRequest("Missing CSV file.");
+            }
+
             try
             {
                 MeterReadingResponse meterReadingResponse;
